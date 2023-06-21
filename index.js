6b7a561156c87ac65388a90b0ad6516436fa8235
Adrianlov <74472740+Adrianlov@users.noreply.github.com>
2023-06-21 12:52:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,40 @@ function removeTransition(e){
 
 const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-console.log(keys)
+
+
+
+
+//For the mobile version
+
+    let click1 = document.getElementById('1')
+    click1.addEventListener('click', function(){
+        let S1 = document.getElementById('snare')
+        S1.currentTime = 0
+        S1.play()
+    })
+   
+    let click2 = document.getElementById('2')
+    click2.addEventListener('click', function(){
+        let S2 = document.getElementById('kick')
+        S2.currentTime = 0
+        S2.play()
+    })
+
+    let click3 = document.getElementById('3')
+    click3.addEventListener('click', function(){
+        let S3 = document.getElementById('hihat')
+        S3.currentTime = 0
+        S3.play()
+    })
+
+    let click4 = document.getElementById('4')
+    click4.addEventListener('click', function(){
+        let S4 = document.getElementById('bass')
+        S4.currentTime = 0
+        S4.play()
+    })
+
  
 
 
